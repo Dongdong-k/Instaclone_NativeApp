35008f470fb9402b8e2d473c071ebbbccdcc50fd
dongdong <crisish@naver.com>
2021-12-14 21:38:17 +0900

    #14.2 Navigator Props (11:43)

--- navigators/LoggedOutNav.tsx
+++ navigators/LoggedOutNav.tsx
@@ -1,5 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
+import { Settings } from "react-native";
 import CreateAccount from "../screens/CreateAccount";
 import Login from "../screens/Login";
 import Welcome from "../screens/Welcome";
@@ -13,11 +14,21 @@ type RootStackParamList = {
 const Stack = createNativeStackNavigator();
 
 // 스택 네비게이터 반환 필요
+// 제일 상단에 있는 것이 초기화면
 export default function LoggedOutNav() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Welcome" component={Welcome} />
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{
+        presentation: "card",
+        headerBackTitleVisible: false,
+        headerTintColor: "darkgreen",
+        headerTitleAlign: "center",
+        animation: "slide_from_right",
+      }}
+    >
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Welcome" component={Welcome} />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />
     </Stack.Navigator>
   );
