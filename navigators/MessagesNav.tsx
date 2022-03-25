293078727bde63d62160fa4de3e0789ff83b6bda
dongdong <crisish@naver.com>
2022-03-25 20:05:50 +0900

    #18.1 Rooms Screen part One (11:51)

--- navigators/MessagesNav.tsx
+++ navigators/MessagesNav.tsx
@@ -2,12 +2,25 @@ import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
 import Room from "../screens/Room";
 import Rooms from "../screens/Rooms";
+import { Ionicons } from "@expo/vector-icons";
 
 const Stack = createStackNavigator();
 
 export default function MessagesNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerTintColor: "white",
+        headerBackTitleVisible: false,
+        headerBackImage: ({ tintColor }: any) => (
+          <Ionicons color={tintColor} name="close" size={28} />
+        ),
+        headerStyle: {
+          backgroundColor: "black",
+        },
+        headerTitleAlign: "center",
+      }}
+    >
       <Stack.Screen name="Rooms" component={Rooms} />
       <Stack.Screen name="Room" component={Room} />
     </Stack.Navigator>
--- screens/Rooms.tsx
+++ screens/Rooms.tsx
@@ -1,10 +1,48 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { gql, useQuery } from "@apollo/client";
+import { FlatList, Text, View } from "react-native";
+import { ROOM_FRAGMENT } from "../Fragments";
+import styled from "styled-components/native";
+import ScreenLayout from "../components/ScreenLayout";
+
+const SEE_ROOMS_QUERY = gql`
+  query seeRooms {
+    seeRooms {
+      ...RoomParts
+    }
+  }
+  ${ROOM_FRAGMENT}
+`;
+
+const RoomContainer = styled.View`
+  background-color: black;
+`;
+
+const RoomText = styled.Text`
+  color: white;
+`;
 
 export default function Rooms() {
+  const { data, loading } = useQuery(SEE_ROOMS_QUERY);
+  console.log("Show Query Result : ", data);
+  const renderItem = ({ item: room }: any) => {
+    return (
+      <RoomContainer>
+        <RoomText>
+          {room.unreadTotal === "0"
+            ? "Name of the other Person"
+            : `${room.unreadTotal} messages`}
+        </RoomText>
+      </RoomContainer>
+    );
+  };
   return (
-    <View>
-      <Text>Rooms Screen</Text>
-    </View>
+    <ScreenLayout loading={loading}>
+      <FlatList
+        data={data?.seeRooms}
+        keyExtractor={(room) => "" + room.id} // number 아니라 string 입력 필요
+        renderItem={renderItem}
+      />
+    </ScreenLayout>
   );
 }
